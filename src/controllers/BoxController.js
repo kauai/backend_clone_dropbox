73f70417586bb3bd54865357755b8f50fcaf93ca
Thiago kauai <html5ephp@gmail.com>
2019-04-09 11:01:59 -0300

    salvanco no banco route /boxes funcionando

--- src/controllers/BoxController.js
+++ src/controllers/BoxController.js
@@ -1,10 +1,15 @@
 const Box = require('../models/Box')
 
 class BoxController{
-    async store(req,res){
-       const box = await Box.create({title:'RocketSeat'})
+    async store(req, res){
+        const box = await Box.create(req.body)
         return res.json(box)
     }
+
+    // async store({ body: { title }}, res){
+    //     const box = await Box.create({ title })
+    //     return res.json(box)
+    // }
 }
 
 module.exports = new BoxController()
