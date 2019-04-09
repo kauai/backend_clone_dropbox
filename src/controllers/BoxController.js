e528e76bd8834768b58666978b2ba5f65481fd5a
Thiago kauai <html5ephp@gmail.com>
2019-04-09 13:16:21 -0300

    rotas implementadas salvando file no banco,upload implementado

--- src/controllers/BoxController.js
+++ src/controllers/BoxController.js
@@ -1,8 +1,18 @@
 const Box = require('../models/Box')
 
 class BoxController{
-    async store(req, res){
+
+    async store(req, res) {
         const box = await Box.create(req.body)
+        return res.json(box)
+    }
+
+    async show(req,res) {
+        // const box = await Box.findById(req.params.id).populate('files')
+        const box = await Box.findById(req.params.id).populate({
+            path:'files',
+            options:{sort: {createdAt: -1 }}
+        })
         return res.json(box)
     }
 
