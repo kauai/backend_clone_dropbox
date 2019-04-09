5cf1c0477a284a6a234dcf946c2cebbb3e99d29e
Thiago kauai <html5ephp@gmail.com>
2019-04-09 15:01:08 -0300

    erro no heroku 01 save commit

--- src/server.js
+++ src/server.js
@@ -2,12 +2,11 @@ const express = require("express");
 const mongoose = require('mongoose')
 const path = require('path')
 const app = express();
-
-app.use(cors)
-
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
 const cors = require('cors')
+
+app.use(cors())
 
 io.on('connection', socket => {
     socket.on('connectRoom',box => {
